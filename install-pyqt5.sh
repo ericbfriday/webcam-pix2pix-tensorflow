5cb4838e1a2dfe52c7294cf4c532b0e244af2e0d
linuxbox <linuxbox@linuxbox1-1804.usinternet.com>
2018-11-17 20:37:31 -0600

    Dockerfile claims dependencies are met for initial app load

---
+++ install-pyqt5.sh
@@ -0,0 +1,22 @@
+PYQT_VERSION=5.7
+SIP_VERSION=4.18.1
+
+cd /tmp
+
+# Install sip
+wget --retry-connrefused https://sourceforge.net/projects/pyqt/files/sip/sip-$SIP_VERSION/sip-$SIP_VERSION.tar.gz
+tar -xzvf sip-$SIP_VERSION.tar.gz
+cd sip-$SIP_VERSION
+python3 configure.py
+make && make install
+cd /tmp
+rm -rf sip-$SIP_VERSION*
+
+# Install PyQt5
+wget --retry-connrefused https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-$PYQT_VERSION/PyQt5_gpl-$PYQT_VERSION.tar.gz
+tar -xzvf PyQt5_gpl-$PYQT_VERSION.tar.gz
+cd PyQt5_gpl-$PYQT_VERSION
+python3 configure.py --confirm-license
+make && make install
+cd /tmp
+rm -rf PyQt5_gpl-$PYQT_VERSION*
